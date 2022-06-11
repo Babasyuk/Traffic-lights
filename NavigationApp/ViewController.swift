9e0d49957a30694da8d430b52a63873ecf474485
Babasyuk <artem.babasyuk@mail.ru>
2022-06-11 12:00:25 +0300

    Обновил данные

--- NavigationApp/ViewController.swift
+++ NavigationApp/ViewController.swift
@@ -8,11 +8,47 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // Ссылка на сториборд, где размещен данный View Controller
+    let storyboardInstanse = UIStoryboard(name: "Main", bundle: nil)
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        navigationItem.title = "Заголовок сцены"
+        navigationItem.backButtonTitle = "Назад"
+        }
+    
+    // перейти к зеленой сцене
+    @IBAction func toGreenScene(_ sender: UIButton) {
+        // получаем ссылку на следующий контроллер
+        // в данном случае следующий - это зеленый
+        let nextViewController = storyboardInstanse.instantiateViewController(withIdentifier: "greenViewController")
+        
+        // обращаемся к Navigation Controller
+        // и вызываем метод перехода к новому контроллеру
+        self.navigationController?.pushViewController(nextViewController, animated: true)
     }
+    
+    // перейти к желтой сцене
+    @IBAction func toYellowScene(_ sender: UIButton) {
+        let nextViewController = storyboardInstanse.instantiateViewController(withIdentifier: "yellowViewController")
+        self.navigationController?.pushViewController(nextViewController, animated: true)
+    }
+    
+    // перейти к корневой сцене
+    @IBAction func toRootScene(_ sender: UIButton) {
+        // обращаемся к Navigation Controller
+        // и вызываем метод перехода к корневому контроллеру
+        self.navigationController?.popToRootViewController(animated: true)
+    }
+    
+    // перейти к предыдущему экрану
+    @IBAction func toPreviousScene(_ sender: UIButton) {
+        self.navigationController?.popViewController(animated: true)
+    }
+        
+
+
 
 
 }
